5290060713b149ab5bec835ff9cba56afaa177d6
nachelysanchez <nashely-5@hotmail.com>
2022-05-09 21:34:21 -0400

    Agregando la entidad Persona.kt

---
+++ app/src/main/java/com/example/personregister/model/Persona.kt
@@ -0,0 +1,12 @@
+package com.example.personregister.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Personas")
+data class Persona(
+    @PrimaryKey(autoGenerate = true)
+    val PersonaId: Int,
+    val Nombres: String,
+    val Balance: Float
+)
